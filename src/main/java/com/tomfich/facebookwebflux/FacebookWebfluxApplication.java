d97f4987b2fa71c320c0e32ebf523c8391a53005
tomWindwos <fichtner.dk@hotmail.com>
2022-05-11 15:36:22 -0300

    Include Project base

---
+++ src/main/java/com/tomfich/facebookwebflux/FacebookWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.tomfich.facebookwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomfich/facebookwebflux/FacebookWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomfich.facebookwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
